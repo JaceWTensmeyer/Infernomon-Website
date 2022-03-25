36a0eb8abdc43a1c21e71408e586e22f2a827220
JaceWTensmeyer <jtenz@live.com>
2022-03-25 16:43:08 -0600

    add some attacks and infernomon

--- infernomon-ui/src/Components/About/About.jsx
+++ infernomon-ui/src/Components/About/About.jsx
@@ -1,7 +1,60 @@
 import React from 'react'
 
+import AboutText from '../../jsonData/aboutText.json'
+
+import './About.scss';
+
 function About() {
-    return <div>about page</div>
+    return <div className='container'>
+        <div className='section'>
+            <div className='sectionHeader'>Introduction</div>
+            <div className='sectionText'>
+                {AboutText.introduction}
+            </div>
+        </div>
+        <div className='section'>
+            <div className='sectionHeader'>Infernomon</div>
+            <div className='sectionText'>
+                {AboutText.infernomon}
+            </div>
+        </div>
+        <div className='section'>
+            <div className='sectionHeader'>Dungeons</div>
+            <div className='sectionText'>
+                {AboutText.dungeons}
+            </div>
+        </div>
+        <div className='section'>
+            <div className='sectionHeader'>Battling</div>
+            <div className='sectionText'>
+                 {AboutText.battling}
+            </div>
+        </div>
+        <div className='section'>
+            <div className='sectionHeader'>Majors</div>
+            <div className='sectionText'>
+                {AboutText.majors}
+            </div>
+        </div>
+        <div className='section'>
+            <div className='sectionHeader'>Auction House</div>
+            <div className='sectionText'>
+                {AboutText.auctionHouse}
+            </div>
+        </div>
+        <div className='section'>
+            <div className='sectionHeader'>Attacks</div>
+            <div className='sectionText'>
+                {AboutText.attacks}
+            </div>
+        </div>
+        <div className='section'>
+            <div className='sectionHeader'>Items</div>
+            <div className='sectionText'>
+                {AboutText.items}
+            </div>
+        </div>
+    </div>
 }
 
 export default About
