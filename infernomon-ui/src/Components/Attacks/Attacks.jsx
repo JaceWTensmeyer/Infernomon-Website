a808ae755ac77cf8b5c207299b2b14d525cc9b24
JaceWTensmeyer <jtenz@live.com>
2022-04-10 23:26:31 -0600

    putting attacks in a table

--- infernomon-ui/src/Components/Attacks/Attacks.jsx
+++ infernomon-ui/src/Components/Attacks/Attacks.jsx
@@ -1,7 +1,108 @@
 import React from 'react'
+import { useTable } from 'react-table'
+
+import attackJSON from '../../jsonData/attacks.json';
 
 function Attacks() {
-    return <div>Attacks page</div>
+    const data = React.useMemo(
+        () => 
+            attackJSON
+    ,[])
+
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Name',
+                accessor: 'name', // accessor is the "key" in the data
+            },
+            {
+                Header: 'Type',
+                accessor: 'type',
+            },
+            {
+                Header: 'Damage',
+                accessor: 'damage',
+            },
+            {
+                Header: 'Atk Category',
+                accessor: 'attackCategory',
+            },
+            {
+                Header: 'Dmg Category',
+                accessor: 'damageCategory',
+            },
+            {
+                Header: 'Description',
+                accessor: 'description',
+            },
+            {
+                Header: 'Cooldown',
+                accessor: 'cooldown',
+            },
+            {
+                Header: 'Cast Duration (seconds)',
+                accessor: 'castDuration',
+            },
+            {
+                Header: 'Range',
+                accessor: 'range',
+            },
+        ],
+        []
+    ) 
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({ columns, data })
+
+    return (
+        <table {...getTableProps()} style={{fontSize: '10px'}}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                    <th
+                        {...column.getHeaderProps()}
+                        style={{
+                        background: 'aliceblue',
+                        color: 'black',
+                        fontWeight: 'bold',
+                        }}
+                    >
+                        {column.render('Header')}
+                    </th>
+                    ))}
+                </tr>
+                ))}
+            </thead>
+       <tbody {...getTableBodyProps()}>
+         {rows.map(row => {
+           prepareRow(row)
+           return (
+             <tr {...row.getRowProps()}>
+               {row.cells.map(cell => {
+                 return (
+                   <td
+                     {...cell.getCellProps()}
+                     style={{
+                       padding: '10px',
+                       border: 'solid 1px gray',
+                     }}
+                   >
+                     {cell.render('Cell')}
+                   </td>
+                 )
+               })}
+             </tr>
+           )
+         })}
+       </tbody>
+     </table>
+    )
 }
 
 export default Attacks
