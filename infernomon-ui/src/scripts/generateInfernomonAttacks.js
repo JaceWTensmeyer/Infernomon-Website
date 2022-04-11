1623250e74ae9051fed5a61fae076baae6efeca4
Jace <jtenz@live.com>
2022-04-10 18:57:10 -0600

    attack script added. added id's to attacks

---
+++ infernomon-ui/src/scripts/generateInfernomonAttacks.js
@@ -0,0 +1,39 @@
+const attacks = require('../jsonData/attacks.json');
+const fs = require('fs');
+
+let txtString = `
+function GetInfernomonAttack(_id){
+	switch(_id) {
+		case 0: break`;
+let i = 1;
+attacks.forEach((atk) => {
+	txtString += `
+		case ${i}: return {
+			id: ${atk.id},
+			attackCategory: "${atk.attackCategory}",
+			damageCategory: "${atk.damageCategory}",
+			name: "${atk.name}",
+			description: "${atk.description}",
+			castType: "${atk.castType}",
+			castDuration: ${atk.castDuration},
+			range: ${atk.range},
+			type: "${atk.type}",
+			damage: ${atk.damage},
+			cooldown: ${atk.cooldown},
+			statusEffect: "${atk.statusEffect}",
+			vstatusEffectChance: ${atk.vstatusEffectChance},
+			statusEffectDuration: ${atk.statusEffectDuration}
+		} break`;
+	i++;
+});
+
+txtString += `
+		default: break
+	}
+}
+`;
+
+fs.writeFileSync(
+	`A:/Projects/Infernomon-Website/infernomon-ui/src/scripts/outputTxt/attackSwitch.txt`,
+	txtString
+);
--- infernomon-ui/src/scripts/generateInfernomonSwitch.js
+++ infernomon-ui/src/scripts/generateInfernomonSwitch.js
@@ -1,6 +1,5 @@
 const infernomon = require('../jsonData/Infernomon.json');
 const fs = require('fs');
-const { cwd } = require('process');
 
 let txtString = `
 function GetGenericInfernomonData(_number){
@@ -14,13 +13,18 @@ infernomon.forEach((inf) => {
 			number: _number,
 			maxExpPoints: 1000000,
 			baseHp: ${inf.hp},
-			baseAttack: ${inf.attack}
-			baseDefense: ${inf.defense}
-			baseSpAttack: ${inf.spAttack}
-			baseSpDefense: ${inf.spDefense}
-			baseSpeed: ${inf.speed}
-			type1: "${inf.type1}"
-			type2: "${inf.type2}"
+			baseAttack: ${inf.attack},
+			baseDefense: ${inf.defense},
+			baseSpAttack: ${inf.spAttack},
+			baseSpDefense: ${inf.spDefense},
+			baseSpeed: ${inf.speed},
+			type1: "${inf.type1}",
+			type2: "${inf.type2}",
+			autoAttack1Id: ${inf.autoAttack1Id},
+			autoAttack2Id: ${inf.autoAttack2Id},
+			naturalAttack1Id: ${inf.naturalAttack1Id},
+			naturalAttack2Id: ${inf.naturalAttack2Id},
+			naturalAttack3Id: ${inf.naturalAttack3Id}
 		} break`;
 	i++;
 });
